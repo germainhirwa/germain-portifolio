2616923ee08b34cefd105275bdf7944579ebdefd
Germain Hirwa <higermain1@gmail.com>
2024-09-13 19:08:17 -0400

    slight change

--- app/page.jsx
+++ app/page.jsx
@@ -26,7 +26,7 @@ const Home = () => {
             {/* btn and socials */}
             <div className="flex flex-col xl:flex-row items-center gap-8">
               <a
-                href="https://drive.google.com/uc?export=download&id=1gYWUDMiWA7gxVyl6q7he1X-Uo5-8koIw"
+                href="https://drive.google.com/uc?export=download&id=1BU_K6umJEcOjhwdIvMYM_AWftAzOugEo"
                 target="_blank"
                 rel="noopener noreferrer"
               >
